84a43ce9814eb023f2c3944864ff945e650b5a3c
ewertonlx <ewerton123ofc@gmail.com>
2024-06-12 23:19:52 -0300

    Modified Schemas

--- Schemas/user.js
+++ Schemas/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const user = new mongoose.Schema({
     _id: {type: String, required: true},
+    language: {type: String, default: 'br'},
 })
 module.exports = mongoose.model('Users', user)

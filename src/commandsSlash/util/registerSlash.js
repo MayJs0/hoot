7dee7244d237f1855a72162c72c03085d3479b39
ewertonlx <ewerton123ofc@gmail.com>
2024-07-09 11:02:12 -0300

    chore: New message register

--- src/commandsSlash/util/registerSlash.js
+++ src/commandsSlash/util/registerSlash.js
@@ -9,15 +9,15 @@ module.exports = {
     const text = {
       br: {
         errorMsg: "você já está registrado no banco de dados!",
-        messageSend: "Registrado com sucesso!",
+        messageSend: `${emojis.emojis.correct} | ${interaction.user}, você foi registrado com sucesso em meu banco de dados! Agora você pode usar todos os meus comandos, utilize </help:1250510627642806342> para ver todos os comandos disponíveis!`,
       },
       en: {
         errorMsg: "you are already registered in the database!",
-        messageSend: "Registred successfully!",
+        messageSend: `${emojis.emojis.correct} | ${interaction.user}, you have been successfully registered in my database! Now you can use all my commands, use </help:1250510627642806342> to see all available commands!`,
       },
     }
     if (userdb)return interaction.reply(`${emojis.emojis.warn} | ${interaction.user}, ${text[userdb.language].errorMsg}!`);
-    interaction.reply({ content: `${emojis.emojis.correct} | ${text[userdb?.language ?? 'br'].messageSend}`});
+    interaction.reply({ content: `${text[userdb?.language ?? 'br'].messageSend}`});
     await User.create({ _id: interaction.user.id });
   },
 };
